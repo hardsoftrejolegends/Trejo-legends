c75851eea770745166d2c9a02dabb433d21b6cfa
Irving <irvingrocharesendiz@gmail.com>
2013-11-04 17:18:26 -0600

    Create izquierda.cs

---
+++ izquierda.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class izquierda : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+		if(Input.touches.Length <= 0){
+			//No hay toque sobre la GUI
+		}else{
+			if(this.guiTexture != null && (this.guiTexture.HitTest(Input.GetTouch(0).position))){
+				if(Input.GetTouch(0).phase == TouchPhase.Began){
+					animation.CrossFade("TCaminar");
+					this.transform.Translate(Vector3.back*5*Time.deltaTime);
+				}
+			}
+		}
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			Application.Quit();
+		}
+	}
+}
