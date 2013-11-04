f583087e9b599e28bcff84d7a80bd4c70fcd9809
Irving <irvingrocharesendiz@gmail.com>
2013-11-04 17:01:10 -0600

    Update colision1.js

--- colision1.js
+++ colision1.js
@@ -1,10 +1,9 @@
- #pragma strict
+  #pragma strict
 var i=0;
 var mensaje = "Golpe";
 var v100 : GUITexture;
 var ko : GUITexture;
-var perdido : GUITexture;
-var perder : Texture2D;
+var ko2 : Texture2D;
 var v90 : Texture2D;
 var v80 : Texture2D;
 var v70 : Texture2D;
@@ -14,7 +13,6 @@ var v40 : Texture2D;
 var v30 : Texture2D;
 var v20 : Texture2D;
 var v10 : Texture2D;
-var ko2 : Texture2D;
 
 function Start () {
 
@@ -25,7 +23,7 @@ function Update () {
 
 function OnCollisionEnter(collision : Collision) {
 		// Debug-draw all contact points and normals
-		if(collision.gameObject.name == "TREJOANIMACIONESerie"){
+		if(collision.gameObject.name == "chihuahuaTODO"){
 		i= i+1;
 		Debug.Log("hit");
 		if (i==1){
@@ -56,11 +54,9 @@ function OnCollisionEnter(collision : Collision) {
 			v100.texture = v10;	
 			}
 		if (i==10){
-			ko.texture = ko2;
-			yield WaitForSeconds (2);
-			perdido.texture = perder;
-			yield WaitForSeconds (2);
-			Application.LoadLevel("Main");
+		ko.texture = ko2;
+		yield WaitForSeconds (2);
+			Application.LoadLevel("Principal");
 			}
 	}
 }
