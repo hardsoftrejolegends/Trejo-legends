3b86f6fca15ef2b35ed75ca7e0996faea908fbce
Irving <irvingrocharesendiz@gmail.com>
2013-11-05 17:40:33 -0600

    Create TouchManager.cs
    /*/
     * Script by Devin Curry
     * www.Devination.com
     * www.youtube.com/user/curryboy001
     * Please like and subscribe if you found my tutorials helpful :D
    /*/

---
+++ TouchManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+ 
+public class TouchManager : TouchLogic 
+{
+ public TouchLogic[] touches2Manage;
+ 
+ void OnTouchEndedAnywhere()
+ {
+  foreach(TouchLogic obj in touches2Manage)
+   if(obj.touch2Watch > TouchLogic.currTouch)
+    obj.touch2Watch--;
+ }
+}
