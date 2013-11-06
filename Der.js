a26de306e1b12b98106b5d1aea0b543eb4cc4c0e
Irving <irvingrocharesendiz@gmail.com>
2013-11-05 20:50:56 -0600

    Create Der.js

---
+++ Der.js
@@ -0,0 +1,16 @@
+function Start(){
+	collider.gameObject.SetActive(false);
+}
+
+function OnTriggerEnter(other : Collider){
+
+	if(other.tag=="DanTM"){
+//		barraVida.bajarVida(10, true);
+	}
+	
+}
+function Activar(x : float){
+	collider.gameObject.SetActive(true);
+	yield WaitForSeconds(0.8);
+	collider.gameObject.SetActive(false);
+}
