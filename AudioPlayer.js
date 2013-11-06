d1453bfda133fff32ac028ff7d31679aabc7b698
Irving <irvingrocharesendiz@gmail.com>
2013-11-05 21:41:05 -0600

    Update AudioPlayer.js

--- AudioPlayer.js
+++ AudioPlayer.js
@@ -2,7 +2,7 @@
 var hSliderValue : float = 0.6;
 
 function OnGUI(){
-	hSliderValue = GUI.HorizontalSlider(Rect(300, 210, 400, 120), hSliderValue, 0.0, 1.0);
+	hSliderValue = GUI.HorizontalSlider(Rect(300, 210, 400, 150), hSliderValue, 0.0, 1.0);
 	GUI.Label(Rect(750, 210, 200, 20), (hSliderValue * 100) + "%");
 	audio.volume = hSliderValue;
 }
