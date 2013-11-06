406bdd71e3a78dcaf8a287bd1248527c6243cd0b
Irving <irvingrocharesendiz@gmail.com>
2013-11-05 21:00:52 -0600

    Update Selection.js

--- Selection.js
+++ Selection.js
@@ -5,40 +5,60 @@
 var BtnCP : GUITexture;
 var BtnBP : GUITexture;
 var BtnBack : GUITexture;
-
-var TiempoFade : float=0.4;
-var TiempoEspera : float=0;
-
-function fLogo () {
-	
-	yield Fade.use.Alpha(guiTexture, 0.0, 1.0, TiempoFade, EaseType.In);
-	yield WaitForSeconds(TiempoEspera);
-	
-}
+//Networking
+var remoteIP = "";
+var remotePort = 25000;
+var listenPort = 25000;
+var useNAT = false;
+var yourIP = "";
+var yourPort = "";
+var extPort = "";
+var extIP = "";
 
 function Start () {
-
-	fLogo();
 }
 
 function Update () {
-	if(Input.touches.Length <= 0){
-		//No hay toque sobre la GUI
+	if (Network.peerType == NetworkPeerType.Disconnected){
+		if(Input.touches.Length <= 0){
+			//No hay toque sobre la GUI
+		}else{
+			if(BtnCP.guiTexture != null && (BtnCP.guiTexture.HitTest(Input.GetTouch(0).position))){
+				if(Input.GetTouch(0).phase == TouchPhase.Began){
+					// Creating server
+					Network.InitializeServer(32, listenPort, useNAT);
+				}
+			}
+			if(BtnBP.guiTexture != null && (BtnBP.guiTexture.HitTest(Input.GetTouch(0).position))){
+				if(Input.GetTouch(0).phase == TouchPhase.Began){
+					Application.LoadLevel("Oponentes");
+				}
+			}
+			if(BtnBack.guiTexture != null && (BtnBack.guiTexture.HitTest(Input.GetTouch(0).position))){
+				if(Input.GetTouch(0).phase == TouchPhase.Began){
+					Application.LoadLevel("Main");
+				}
+			}
+		}
 	}else{
-		if(BtnCP.guiTexture != null && (BtnCP.guiTexture.HitTest(Input.GetTouch(0).position))){
-			if(Input.GetTouch(0).phase == TouchPhase.Began){
-				Application.LoadLevel("Fondo");
-			}
-		}
-		if(BtnBP.guiTexture != null && (BtnBP.guiTexture.HitTest(Input.GetTouch(0).position))){
-			if(Input.GetTouch(0).phase == TouchPhase.Began){
-				Application.LoadLevel("Oponentes");
-			}
-		}
-		if(BtnBack.guiTexture != null && (BtnBack.guiTexture.HitTest(Input.GetTouch(0).position))){
-			if(Input.GetTouch(0).phase == TouchPhase.Began){
-				Application.LoadLevel("Main");
-			}
+		// Getting your ip address and port
+		yourIP = Network.player.ipAddress;
+		yourPort = Network.player.port.ToString();
+		extPort = Network.player.externalPort.ToString();
+		extIP = Network.player.externalIP;
+		
+		GUI.Label(new Rect(150,20,300,100),"IP Adress: "+yourIP+" : "+yourPort + " -- " + extPort + "--" + extIP);
+		if (GUI.Button (new Rect(10,10,100,50),"Disconnect"))
+		{
+			// Disconnect from the server
+			Network.Disconnect(200);
 		}
 	}
+}
+
+function OnConnectedToServer () {
+	// Notify our objects that the level and the network are ready
+	for (var go : GameObject in FindObjectsOfType(GameObject))
+		go.SendMessage("OnNetworkLoadedLevel",
+	SendMessageOptions.DontRequireReceiver);
 }
