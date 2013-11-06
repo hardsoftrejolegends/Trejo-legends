656cc72be991c7d0e5f899d5335bafebdb0b538e
Irving <irvingrocharesendiz@gmail.com>
2013-11-05 19:59:58 -0600

    Update Joystick.js
    //////////////////////////////////////////////////////////////
    // Joystick.js
    // Penelope iPhone Tutorial
    //
    // Joystick creates a movable joystick (via GUITexture) that 
    // handles touch input, taps, and phases. Dead zones can control
    // where the joystick input gets picked up and can be normalized.
    //
    // Optionally, you can enable the touchPad property from the editor
    // to treat this Joystick as a TouchPad. A TouchPad allows the finger
    // to touch down at any point and it tracks the movement relatively 
    // without moving the graphic
    //////////////////////////////////////////////////////////////

--- Joystick.js
+++ Joystick.js
@@ -1,17 +1,3 @@
-//////////////////////////////////////////////////////////////
-// Joystick.js
-// Penelope iPhone Tutorial
-//
-// Joystick creates a movable joystick (via GUITexture) that 
-// handles touch input, taps, and phases. Dead zones can control
-// where the joystick input gets picked up and can be normalized.
-//
-// Optionally, you can enable the touchPad property from the editor
-// to treat this Joystick as a TouchPad. A TouchPad allows the finger
-// to touch down at any point and it tracks the movement relatively 
-// without moving the graphic
-//////////////////////////////////////////////////////////////
-
 #pragma strict
 
 @script RequireComponent( GUITexture )
